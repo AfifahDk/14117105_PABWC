cbbb9e7869d8a836d9e60c3adee0a3e0844067f5
Afifah Dwikirani <47997108+AfifahDk@users.noreply.github.com>
2019-11-05 14:44:16 +0700

    Minggu_9

--- Minggu_9/cookies/tes_cookies.php
+++ Minggu_9/cookies/tes_cookies.php
@@ -7,5 +7,12 @@
 	echo "Cookies disabled.";
 	} ?>
 	<!-- analisis disini -->
+	<!--Hal ini terjadi karena Incognito Mode tidak akan menyimpan cache,cookies dan history 
+	dari website yang kita akses. Incognito Mode akan memulai sesi baru ketika menutup window 
+	web browser, berbeda dengan window tanpa Incognito Mode yang akan menyimpan informasi di cache 
+	yang merupakan memori sementara yang bertujuan untuk menyimpan informasi dari website tersebut 
+	sehingga pada saat kita mengakses website tersebut maka kita akan mengakses cache terlebih dulu 
+	baru diteruskan diwebsite tersebut sehingga saat kita mengakses website tersebut akan terasa lebih 
+	cepat karena sudah tersimpan terlebih dahulu.-->
 </body>
 </html>
